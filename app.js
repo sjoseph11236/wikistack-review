5dc17616d734022065c8c8187f73995ee5eb9713
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-22 16:23:50 -0500

    feat: add get method

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // import the express module.
 const express = require('express');
+// Create an express application
 const app = express();
 
+// identify the target port
 const PORT = 1337;
-
+// Create a get route that responds with hello world.
+app.get('/', (req, res ) => {
+  res.send('Hello World');
+})
 // Use the listen method to access the port. 
 app.listen(PORT, () => {
   console.log(`listening on PORT: ${PORT}`);
