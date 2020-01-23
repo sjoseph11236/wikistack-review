e9af2f5e4209fc0b91af597b456efe2dacc75d41
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-23 15:59:50 -0500

    chore: add small comments

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 // import the express module.
 const express = require('express');
-// Create an express application
+// Create an express application instance
 const app = express();
+// A module to log middleware
+const morgan = require('morgan');
+// const bodyParser = require('body-parser');
 
+// middleware that allows us to read the body
+// app.use(bodyParser.urlencoded({extended: false}));
+
+// or
+
+// 
+app.use(express.json());
+app.use(express.urlencoded());
+// use the logging middleware for HTTP request called morgan
+app.use(morgan('dev'));
 // identify the target port
 const PORT = 1337;
-// Create a get route that responds with hello world.
+
+// Create a get route to root patth that responds with hello world.
 app.get('/', (req, res ) => {
   res.send('Hello World');
-})
+});
+
+
+
 // Use the listen method to access the port. 
 app.listen(PORT, () => {
   console.log(`listening on PORT: ${PORT}`);
-})
+});
 
 
 //
