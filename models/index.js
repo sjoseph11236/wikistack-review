435df18d109d176dfd9040cd9885bb91eafb4f87
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-23 20:36:05 -0500

    testing

--- models/index.js
+++ models/index.js
@@ -9,12 +9,15 @@ const db = new Sequelize('postgres://localhost:5432/wikistack', {
 const Page = db.define('page', {
   title: { 
     type: Sequelize.STRING,
+    allowNull: false
   }, 
   slug:{ 
     type: Sequelize.STRING,
+    allowNull: false
   }, 
-  content: { 
+  content: {
     type: Sequelize.TEXT,
+    allowNull: false
   },
   status: { 
     type: Sequelize.ENUM('open', 'closed')
@@ -25,12 +28,19 @@ const Page = db.define('page', {
 const User = db.define('user', {
   name: { 
     type: Sequelize.STRING,
+    allowNull: false
   },
   email: { 
     type: Sequelize.STRING,
+    allowNull: false,
+    validate: { 
+      isEmail: true
+    }
   }
 });
 
 module.exports = {
-  db
+  db, 
+  Page, 
+  User
 };
