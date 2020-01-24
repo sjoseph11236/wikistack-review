8fb41ff4c12a0ad014ebacb16caf981eba9d2286
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-24 10:59:26 -0500

    fix: fix commit

--- views/addPage.js
+++ views/addPage.js
@@ -9,21 +9,21 @@ module.exports = () => layout(html`
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Author Name</label>
       <div class="col-sm-10">
-        <input id="name" name="title" type="text" class="form-control"/>
+        <input  name="name" type="text" class="form-control"/>
       </div>
     </div>
     
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Author Email</label>
       <div class="col-sm-10">
-        <input id="email" name="title" type="text" class="form-control"/>
+        <input name="email" type="text" class="form-control"/>
       </div>
     </div>
     
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Page Title</label>
       <div class="col-sm-10">
-        <input id="title" name="title" type="text" class="form-control"/>
+        <input  name="title" type="text" class="form-control"/>
       </div>
     </div>
 
